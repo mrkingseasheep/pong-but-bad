a85f15f4e14ae4f5fe45ba195a4a6ab73023981d
Xinyang Wang <136667147+mrkingseasheep@users.noreply.github.com>
2024-08-13 14:26:22 -0400

    Just keep this here for reference ig

--- main.cpp
+++ main.cpp
@@ -10,6 +10,32 @@
 #include <SFML/Window/VideoMode.hpp>
 #include <iostream>
 
+/*
+help im being jumped by lifetimes
+i hate this
+https://pvs-studio.com/en/blog/posts/cpp/1006/
+const int& a = 0; // works
+int& b = 0; dont work
+first lifetime gets extended to end of {}
+*/
+
+// uses NRVO nvm doesnt work at all
+// named return value optimization
+// c++17+
+
+// gosh damnitn why are lifetimes
+
+// ight so we just shove it on nthe heap then :P 😔
+sf::Sprite* newSprite(const std::string& fileTexture) {
+    sf::Texture texture;
+    if (!texture.loadFromFile(fileTexture)) {
+        std::cerr << "uh ohhh stinky" << std::endl;
+    }
+    texture.setSmooth(true);
+    sf::Sprite* sprite = new sf::Sprite(texture);
+    return sprite;
+}
+
 const std::string PLAYER_TEXTURE_FILE = "obama.jpg";
 
 class Ball {
